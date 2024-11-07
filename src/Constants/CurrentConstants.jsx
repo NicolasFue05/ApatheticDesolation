411c6e6e787876e35de863d4da7ea3b7bb6be79d
NicolasFue05 <nicolasjfc05@gmail.com>
2024-11-07 17:03:25 -0500

    :version 1.0: Current constants updated and fixed

--- src/Constants/CurrentConstants.jsx
+++ src/Constants/CurrentConstants.jsx
@@ -1,12 +1,83 @@
-
-
 export const notification = [
     {
         username: 'NicolasFue05',
-        message: 'Hey bro, are you going to the party tonight?'
+        message: 'Hey, are you going to the party tonight?'
     },
     {
-        username: '',
-        message: ''
+        username: 'unregistered',
+        message: 'please babe, can we walk about it?'
     }
 ];
+
+export const chatsMessages = [
+    {
+        username: notification[0].username,
+        messages: [
+            {
+                key: 1,
+                isReceived: false,
+                message: 'idk, this is different',
+                time: '11:30 p.m.'
+            },
+            {
+                key: 2,
+                isReceived: true,
+                message: 'How do u really feel?',
+                time: '11:35 p.m.'
+            },
+            {
+                key: 3,
+                isReceived: false,
+                message: "u know I'm always fine.",
+                time: '3:33 a.m.'
+            },
+            {
+                key: 4,
+                isReceived: true,
+                message: "I'm glad to hear it",
+                time: '10:00 a.m.'
+            },
+            {
+                key: 5,
+                isReceived: true,
+                message: notification[0].message,
+                time: '5:00 p.m.'
+            }
+        ]
+    },
+    {
+        username: notification[1].username,
+        messages: [
+            {
+                key: 1,
+                isReceived: false,
+                message: 'come on!!!, they took a photo of you kissing ███████',
+                time: '11:35 p.m.'
+            },
+            {
+                key: 2,
+                isReceived: true,
+                message: 'id noyhing babr',
+                time: '12:20 a.m.'
+            },
+            {
+                key: 3,
+                isReceived: false,
+                message: "you can't even write well...",
+                time: '12:23 a.m.'
+            },
+            {
+                key: 4,
+                isReceived: false,
+                message: "dont text me.",
+                time: '12:23 a.m.'
+            },
+            {
+                key: 5,
+                isReceived: true,
+                message: notification[1].message,
+                time: '5:01 p.m.'
+            }
+        ]
+    }
+]
