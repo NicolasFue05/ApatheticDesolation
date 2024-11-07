3c84f1492b65e7d23c9f370e4d4fc98baf2eea3c
NicolasFue05 <nicolasjfc05@gmail.com>
2024-11-07 17:06:32 -0500

    :version 1.0: Notification updated and fixed

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Notification.css'
 
-export function Notification({ Username, Message, Time = 'Now', className }) {
+export function Notification({ Username, Message, Time = 'Now', className, onClick }) {
+    const displayUsername = Username === 'unregistered' ? Username : `@${Username}`; 
+
     return(
-        <div className={`Notification ${className}`}>
+        <div className={`Notification ${className}`} onClick={onClick}>
             <div className="username-time">
-                <div className="username">@{Username}</div>
+                <div className="username">{displayUsername}</div>
                 <div className="time">{Time}</div>
             </div>
 
